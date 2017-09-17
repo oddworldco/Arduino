1279ce8271d66b721784900f15885d2f068c71ea
rhvrabec <rhvrabec@gmail.com>
2017-09-17 18:24:07 -0500

    Hack around string size limits.

--- temp_DS18S20_string.ino
+++ temp_DS18S20_string.ino
@@ -11,43 +11,84 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
  
-void setup(void)
+void setup()
 {
   // start serial port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
-
   // Start up the library
  sensors.begin();
 }
  
  
-void loop(void)
+void loop()
 {
-  // call sensors.requestTemperatures() to issue a global temperature
-  // request to all devices on the bus
+  // call sensors.requestTemperatures() to issue a global temperature request to all devices on the bus
   //Serial.print(" Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
+
+  
+  for(int i = 0; i < 720; i++)
+{
+  //sensors.requestTemperatures(); // Send the command to get temperatures
   //Serial.println("DONE");
+  //delay(1000);
+  //float temperatureInF = sensors.getTempFByIndex(0);
+  //temperatureInF = (temperatureInF * 100);
+  //char outstr[30];
+  //outstr = sensors.getTempFByIndex(0);
+  //dtostrf(temperatureInF, 8, 4, outstr);
+  //String bodyTempPrint = (outstr);
 
+  sensors.setWaitForConversion(true);  // changed from false
+  sensors.requestTemperatures();
+  sensors.setWaitForConversion(true);
+
+    // Do something else while temp is been reading by the sensor'
   AccelerationReading currentAccel = Bean.getAcceleration();
-
   uint16_t batteryReading = Bean.getBatteryVoltage();
   int beanTemp = Bean.getTemperature();
-  String nameToPrint = "bean_1";
+  String nameToPrint = "bean_2";
   int xToPrint = currentAccel.xAxis;
   int yToPrint = currentAccel.yAxis;
   int zToPrint = currentAccel.zAxis;
+
+   
+  float temp = sensors.getTempFByIndex(0);
+  double a = 99.99;
+  int sizetemp;
+  sizetemp = sizeof(temp);
+  double d;
+  float f;
+  //String s;
+  char s[30];
+  dtostrf(temp, 8, 2, s);
   
+  //Serial.print(AllPrint);
+  //Serial.print(a);
  
-  float temperatureInF = sensors.getTempFByIndex(0);
-  //String tempF = String(temperatureInF, 4);
-  char outstr[25];
-  dtostrf(temperatureInF, 6, 2, outstr);
-  Serial.print("name: "+ nameToPrint+ ", battery: "+ batteryReading+", x: "+ xToPrint+ ", y: "+ yToPrint+ ", z: "+zToPrint+", BodyTemp: "+ String(outstr));
+
+  if ((sizetemp) == 4)
+  {
+  f = temp;
+  d = (double) f;
+  //s = (String) d;
+  
+  //Serial.print(f, 2);
+  //Serial.print(s);
+  //Serial.print(AllPrint+", bodyTemp: "+s);
+  String TempPrint = "name: "+ nameToPrint+", bodyTemp: "+s+", beanTemp:"+beanTemp;
+  String AccelPrint = "name: "+ nameToPrint+", x: "+ xToPrint+ ", y: "+ yToPrint+ ", z: "+zToPrint;
+  String BattPrint = "name: "+ nameToPrint+", battery: "+batteryReading;
+  Serial.print(TempPrint);
+  Serial.print(AccelPrint);
+  Serial.print(BattPrint);
+  }
+  else
+  {
+    continue;
+  }
   //Serial.print(temperatureInF, 4); // Why "byIndex"? 
     // You can have more than one IC on the same bus. 
     // 0 refers to the first IC on the wire
-  Bean.sleep(30000);
- 
+  Bean.sleep(10000);
+ }
 }
