d7e3eb6a54a6a0cb5770ca5cf39b2c3a60d21516
rhvrabec <rhvrabec@gmail.com>
2017-10-30 19:25:31 -0500

    iphone/webapp compatible - heather

--- BetaTestSmartyPants20171022.ino
+++ BetaTestSmartyPants20171022.ino
@@ -10,22 +10,13 @@ OneWire oneWire(ONE_WIRE_BUS);
  
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
-
-ScratchData scratchCommand;
-ScratchData scratchBlank;
-
+ 
 void setup()
 {
   // start serial port
   Serial.begin(9600);
   // Start up the library
  sensors.begin();
-
-  //set up buffer for scratch values
-  uint16_t buffer [10] = { '   ' };
-  
-  //initialize scratch 2 with blank space
-  //Bean.setScratchData(2, buffer, 10);
 }
  
  
@@ -37,45 +28,29 @@ void loop()
   
   for(int i = 0; i < 720; i++)
 {
+  //sensors.requestTemperatures(); // Send the command to get temperatures
+  //Serial.println("DONE");
+  //delay(1000);
+  //float temperatureInF = sensors.getTempFByIndex(0);
+  //temperatureInF = (temperatureInF * 100);
+  //char outstr[30];
+  //outstr = sensors.getTempFByIndex(0);
+  //dtostrf(temperatureInF, 8, 4, outstr);
+  //String bodyTempPrint = (outstr);
+
   sensors.setWaitForConversion(true);  // changed from false
   sensors.requestTemperatures();
+  sensors.setWaitForConversion(true);
 
   // Do something else while temp is been reading by the sensor'
   AccelerationReading currentAccel = Bean.getAcceleration();
   uint16_t batteryReading = Bean.getBatteryVoltage();
   int beanTemp = Bean.getTemperature();
-  String nameToPrint = "bean_2";
+  String nameToPrint = "heather";
   int xToPrint = currentAccel.xAxis;
   int yToPrint = currentAccel.yAxis;
   int zToPrint = currentAccel.zAxis;
-  
-  //new attempt to read into buffer @ bank 2
-  // get temperature
-
-  float fTemp = sensors.getTempFByIndex(0);
-  //Serial.print("F");
-  //Serial.print(fTemp);
-  float fxTemp = fTemp * 100;
-  //Serial.print("Fx");
-  //Serial.print(fxTemp);
-  uint16_t bTemp = (uint16_t) fxTemp;
-  //Serial.print("B");
-  //Serial.print(bTemp);
-  //float aTemp = (float) bTemp;
-  //Serial.print("Fa");
-  //Serial.print(aTemp);
-
-  String strTemp = ""+(String) bTemp;
-  //Serial.print(strTemp);
-  uint8_t bufTemp[16];
-  for ( int i=0; i<strTemp.length(); i++ )
-  {
-    bufTemp [i] = strTemp.charAt(i);
-    //Serial.write(bufTemp[i]);
-  }
-  //write temp array to scratch 2
-  Bean.setScratchData( 2, bufTemp, strTemp.length() );
-
+   
   float temp = sensors.getTempFByIndex(0);
   double a = 99.99;
   int sizetemp;
@@ -102,20 +77,15 @@ void loop()
   //Serial.print(f, 2);
   //Serial.print(s);
   //Serial.print(AllPrint+", bodyTemp: "+s);
-  String TempPrint = "name: "+ nameToPrint+", bodyTemp: "+s+", beanTemp:"+beanTemp;
-  String AccelPrint = "name: "+ nameToPrint+", x: "+ xToPrint+ ", y: "+ yToPrint+ ", z: "+zToPrint;
-  String BattPrint = "name: "+ nameToPrint+", battery: "+batteryReading;
-  
-
-  Serial.print(number);
+  String TempPrint = "n: "+ nameToPrint+", bdy: "+s+", bn:"+beanTemp+", x: "+ xToPrint+ ", y: "+ yToPrint+ ", z: "+zToPrint+", battery: "+batteryReading;
+   
   Serial.print(TempPrint);
-  Serial.print(AccelPrint);
-  Serial.print(BattPrint);
+
   }
   else
   {
     continue;
   }
-  Bean.sleep(10000);
+  Bean.sleep(60000);
  }
 }
